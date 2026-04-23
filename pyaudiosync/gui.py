b627c4a477792f739233e4f19cf595077360f6c2
Nexus <timotej.petrik1@gmail.com>
2026-04-23 20:13:37 +0100

    adjusting the listbox width

--- pyaudiosync/gui.py
+++ pyaudiosync/gui.py
@@ -7,7 +7,7 @@ def gui(device_list):
     Label = tkinter.Label(root, text="PyAudioSync")
     Label.pack()
 
-    listbox = tkinter.Listbox(root, height=20, width=250)
+    listbox = tkinter.Listbox(root, height=20, width=100)
     for i in device_list:
         display_text = f"ID {i['id']}: {i['name']}"
         listbox.insert(tkinter.END, display_text)
