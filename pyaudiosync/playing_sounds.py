c7ee237704a02bf4f973631efe578382e93c4fab
Nexus <timotej.petrik1@gmail.com>
2026-04-25 20:34:19 +0100

    Making it so that unselecting devices doesn't output an error

--- pyaudiosync/playing_sounds.py
+++ pyaudiosync/playing_sounds.py
@@ -2,13 +2,17 @@ import numpy as np
 import sounddevice as sd
 
 
-def play_beep(device_id, frequency=440, duration=5):
+def play_beep(device_id, frequency=440, duration=1):
     fs = 44100
     t = np.linspace(0, duration, int(fs * duration), False)
     beep = 0.5 * np.sin(2 * np.pi * frequency * t)
+    print(f"playing to device {device_id}")
 
-    try:
-        sd.play(beep, samplerate=fs, device=(device_id))
-        sd.wait()
-    except:
-        print("could not play to device (Maybe device is not connected?)")
+    if device_id != 0:
+        try:
+            sd.play(beep, samplerate=fs, device=(device_id))
+            sd.wait()
+        except:
+            print("could not play to device (Maybe device is not connected?)")
+    else:
+        print("No device selected")
